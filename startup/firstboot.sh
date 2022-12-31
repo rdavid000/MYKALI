c891cc8d4296f2d211e804938a843545c4e25e96
unknown <r.david000@gmail.com>
2023-01-01 00:40:55 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get install openssh-server -y
-#apt-get install kali-linux-headless -y 
-apt-get clean -yq
+apt-get install openssh-server sudo -y
+usermod -aG sudo r2d2
+newgrp sudo
 sed -i 's/#*X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
 sed -i 's/#*X11UseLocalhost.*/X11UseLocalhost no/' /etc/ssh/sshd_config
+apt-get install kali-linux-headless  -y
+apt-get clean -yq
 exit 0
