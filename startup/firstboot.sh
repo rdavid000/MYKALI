c6359fefc941720ed0da0549a935623e7f474820
unknown <r.david000@gmail.com>
2022-12-31 21:04:29 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -1,5 +1,5 @@
-export DEBIAN_FRONTEND=noninteractive
-apt-get update -yq && apt-get install git -yq
+apt-get update -yq
+apt-get install git -yq
 apt-get clean -y
 sed -i 's/#*X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
 sed -i 's/#*X11UseLocalhost.*/X11UseLocalhost no/' /etc/ssh/sshd_config
