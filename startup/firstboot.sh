344673d6b2d9c9a059e8595aae13dd76c5647fcb
unknown <r.david000@gmail.com>
2022-12-31 21:52:05 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -7,4 +7,3 @@ service ssh restart
 cat /etc/ssh/sshd_config
 sed -i 's/#*X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
 sed -i 's/#*X11UseLocalhost.*/X11UseLocalhost no/' /etc/ssh/sshd_config
-exit 1
