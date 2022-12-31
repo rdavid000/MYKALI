d1685ad0f8ee9c2aae411347f58204908aeaf325
unknown <r.david000@gmail.com>
2022-12-31 22:14:12 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get install openssh-server git -y
+apt-get install openssh-server -y
 #apt-get install kali-linux-headless -y 
 apt-get clean -yq
 sed -i 's/#*X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
