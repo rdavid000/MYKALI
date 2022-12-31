fc6c4583a4697d85343249b8126e7aba83db666b
unknown <r.david000@gmail.com>
2022-12-31 20:13:49 +0100

    my

--- startup/start.sh
+++ startup/start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt update && apt -y install git
-mkdir /root/git_install
-cd /root/git_install
-git clone https://github.com/rdavid000/MYKALI.git
-RUN sed -i 's/#*X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
-RUN sed -i 's/#*X11UseLocalhost.*/X11UseLocalhost no/' /etc/ssh/sshd_config
+echo "HELLO MYKALI\n"
 exit 1
