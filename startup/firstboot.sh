0fb35feb22406d082816f5b26164d9aca6e7039d
Pharah <r.david000@gmail.com>
2023-02-06 01:08:36 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -6,7 +6,6 @@ usermod -aG sudo r2d2
 newgrp sudo
 sed -i 's/#*X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
 sed -i 's/#*X11UseLocalhost.*/X11UseLocalhost no/' /etc/ssh/sshd_config
-apt-get install kali-linux-headless  -y
+apt-get install kali-linux-headless -y
 apt-get install kali-linux-everything -y
 apt-get clean -yq
-exit 0
