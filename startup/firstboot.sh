d852a879150252dc0d576d1a4630b04dae16b640
Pharah <r.david000@gmail.com>
2023-02-06 14:10:03 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt upgrade -y
-apt install openssh-server sudo -y
+apt install openssh-server sudo wget -y
 usermod -aG sudo r2d2
 newgrp sudo
 sed -i 's/#*X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
@@ -12,4 +12,9 @@ sed -i 's/#*X11UseLocalhost.*/X11UseLocalhost no/' /etc/ssh/sshd_config
 apt install kali-linux-headless -y
 apt install kali-linux-everything -y
 apt install inetutils-ping nano -y
+mkdir /home/r2d2/hack/git -p
+wget https://raw.githubusercontent.com/internetwache/GitTools/master/Dumper/gitdumper.sh -O /home/r2d2/hack/git/gitdumper.sh
+chmod +x /home/r2d2/hack/git/gitdumper.sh
+wget https://raw.githubusercontent.com/internetwache/GitTools/master/Extractor/extractor.sh -O /home/r2d2/hack/git/extractor.sh
+chmod +x /home/r2d2/hack/git/extractor.sh
 apt clean -yq
