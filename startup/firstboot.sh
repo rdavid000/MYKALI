6e4fe92413f58d13ae0856790f39e240b688fadb
Pharah <r.david000@gmail.com>
2023-02-06 13:19:38 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo "toto"
 export DEBIAN_FRONTEND=noninteractive
-echo "toto"
+export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-echo "toto"
+apt-get upgrade -y
 apt-get install openssh-server sudo -y
 usermod -aG sudo r2d2
 newgrp sudo
 sed -i 's/#*X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
 sed -i 's/#*X11UseLocalhost.*/X11UseLocalhost no/' /etc/ssh/sshd_config
-echo "toto"
 apt-get install kali-linux-headless -y
 apt-get install kali-linux-everything -y
 apt-get clean -yq
