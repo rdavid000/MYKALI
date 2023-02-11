3223617742664e16ca817385e754960c45219749
Pharah <r.david000@gmail.com>
2023-02-11 19:41:58 +0100

    my

--- startup/firstboot.sh
+++ startup/firstboot.sh
@@ -2,7 +2,7 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt upgrade -y
-apt install openssh-server sudo wget -y
+apt install openssh-server sudo wget inetutils-ping nano -y
 usermod -aG sudo r2d2
 newgrp sudo
 sed -i 's/#*X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
@@ -25,3 +25,16 @@ wget https://raw.githubusercontent.com/The-Z-Labs/linux-exploit-suggester/master
 chmod +x /home/r2d2/hack/privesc/linux-exploit-suggester.sh
 wget https://raw.githubusercontent.com/Cerbersec/scripts/master/linux/linpeas.sh -O /home/r2d2/hack/privesc/linpeas.sh
 chmod +x /home/r2d2/hack/privesc/linpeas.sh
+wget https://github.com/wildkindcc/Exploitation/blob/4d28fb2a7e96d814c2b66dff71376476e7f88b56/00.PostExp_Linux/pspy/pspy64 -O /home/r2d2/hack/privesc/pspy64
+chmod +x /home/r2d2/hack/privesc/pspy64
+wget https://github.com/wildkindcc/Exploitation/blob/4d28fb2a7e96d814c2b66dff71376476e7f88b56/00.PostExp_Linux/pspy/pspy32 -O /home/r2d2/hack/privesc/pspy32
+chmod +x /home/r2d2/hack/privesc/pspy32
+mkdir /home/r2d2/hack/revshell -p
+wget https://github.com/int0x33/nc.exe/blob/fa87aa42c460d34966efb998a1788efca6db11a7/nc64.exe -O /home/r2d2/hack/revshell/nc64.exe
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+gpg --dearmor
+sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main'
+sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt update
+sudo apt install codium
